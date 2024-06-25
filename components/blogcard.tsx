cb473651342b54f8474d4d358144e896680dda00
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-25 10:25:35 +0300

    Blog card

---
+++ components/blogcard.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+
+type Props = {
+    title: string;
+    description: string;
+}
+
+const BlogCard = ({ title, description }: Props) => {
+  return (
+    <Card className="w-full md:w-[45%]">
+        <CardHeader>
+            <CardTitle>{title}</CardTitle>
+        </CardHeader>
+        <CardContent>
+            <p>{description}</p>
+        </CardContent>
+        <CardFooter>
+            <Link className="font-bold" href="/">Read more</Link>
+        </CardFooter>
+    </Card>
+  );
+};
+
+export default BlogCard;
