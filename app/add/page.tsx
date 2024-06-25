6f821d13c4750afb65a5d663c6aff84c5299e0a8
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-25 15:37:35 +0300

    Basic add blog page

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,3 +1,10 @@
+import { Textarea } from "@/components/ui/textarea";
+
 export default function AddBlog() {
-    return <h1>Add a blog</h1>
+    return (
+        <div className="w-full flex gap-x-2 p-2 h-[80vh]">
+            <Textarea className="resize-none w-1/2" placeholder="Markdown" />
+            <Textarea className="resize-none w-1/2" placeholder="Formatted"disabled />
+        </div>
+    )
 ;}
