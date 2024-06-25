ec2636f709d0f63b26781bba6a82575c6b44aec2
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-25 10:45:23 +0300

    Specific blog page

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,12 @@
+export default function BlogPage() {
+    return (
+        <div className="w-full">
+            <div className="h-48 p-8 bg-primary flex items-end">
+                <h1 className="text-2xl font-bold text-white">Understanding the JavaScript Modulo Operator</h1>
+            </div>
+            <div className="p-8">
+                <p>This year, the React team unveiled something they've been quietly researching for years: an official way to run React components exclusively on the server. This is a significant paradigm shift, and it's caused a whole lot of confusion in the React community. In this tutorial, we'll explore this new world, and build an intuition for how it works, and how we can take advantage of it.</p>
+            </div>
+        </div>
+    );
+};
