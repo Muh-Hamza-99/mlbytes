3929b4d01bf9325618f3cb070dc99421a213119e
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-26 00:04:18 +0300

    Custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="w-full h-[80vh] flex justify-center items-center text-5xl text-primary font-extrabold">
+            Not Found 😕
+        </div>
+    );
+};
