3843acf3b69410dc995830ae4091803da3822fe5
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-25 10:57:53 +0300

    Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: Readonly<{children: React.React
         <div className="w-full">
             <Navbar />
             {children}
+            <Footer />
         </div>
       </body>
     </html>
---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full text-sm py-4 border-t flex justify-center text-primary">
+        &copy; Muhammad Hamza 2024
+    </footer>
+  );
+};
+
+export default Footer;
