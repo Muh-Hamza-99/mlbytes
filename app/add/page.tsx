4d8ff6d4a02b0dedcb4f06de423a6ae9a718e40d
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-25 22:53:03 +0300

    Realtime compilation html to markdown

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,10 +1,28 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+import Showdown from "showdown";
+
+const converter = new Showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true,
+})
 
 export default function AddBlog() {
+    const [content, setContent] = useState("");
     return (
-        <div className="w-full flex gap-x-2 p-2 h-[80vh]">
-            <Textarea className="resize-none w-1/2" placeholder="Markdown" />
-            <Textarea className="resize-none w-1/2" placeholder="Formatted"disabled />
-        </div>
+        <>
+            <div className="w-full flex gap-x-2 p-2 h-[80vh]">
+                <Textarea className="resize-none w-1/2" placeholder="Markdown" value={content} onChange={event => setContent(event.target.value)} />
+                <Textarea className="resize-none w-1/2" placeholder="Formatted" disabled value={converter.makeHtml(content)} />
+            </div>
+            <div className="p-4 flex justify-end">
+                <Button>Publish</Button>
+            </div>
+        </>
     )
 ;}
