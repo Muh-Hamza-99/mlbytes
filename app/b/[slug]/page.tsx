f05d675d5bd16ec6f17f50ebcc2c4f7e22f6924a
Muhammad Hamza <codingtimelapseyt@gmail.com>
2024-06-27 01:36:10 +0300

    Remote MDX implemented with unique slug

--- app/b/[slug]/page.tsx
+++ app/b/[slug]/page.tsx
@@ -1,11 +1,23 @@
-export default function BlogPage() {
+import prisma from "@/lib/prisma";
+import { MDXRemote } from "next-mdx-remote/rsc"
+import { notFound } from "next/navigation";
+
+const testMDX = `# Linear Regression
+
+## Cost Function
+
+## Gradient Descent`
+
+export default async function BlogPage({ params }: { params: { slug: string } } ) {
+    // const blog = await prisma.blog.findUnique({ where: { slug: params.slug } });
+    // if (!blog) return notFound();
     return (
         <div className="w-full">
             <div className="h-48 p-8 bg-primary flex items-end">
                 <h1 className="text-2xl font-bold text-white">Understanding the JavaScript Modulo Operator</h1>
             </div>
             <div className="p-8">
-                <p>This year, the React team unveiled something they've been quietly researching for years: an official way to run React components exclusively on the server. This is a significant paradigm shift, and it's caused a whole lot of confusion in the React community. In this tutorial, we'll explore this new world, and build an intuition for how it works, and how we can take advantage of it.</p>
+                <MDXRemote source={testMDX} />
             </div>
         </div>
     );
